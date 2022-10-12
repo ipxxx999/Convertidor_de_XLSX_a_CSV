9febae578e33aaaf3b631e555a78f3af90f57ba2
ipxxx999 <tutovideo2000@gmail.com>
2022-10-12 12:24:23 -0400

    Convertidor de XLSX a CSV

---
+++ Xlsx_a_Csv_view.php
@@ -0,0 +1,51 @@
+<?php
+// This file contains no error or security checking. Use only for internal purposes or in trusted environments.
+// You will need to customise lines 12 and 18 (the two starting "$csv[]=array") to your desired output.
+// Optionally change the output filename in line 22.
+// The example included is for converting a futures trading record from xlsx to csv for import to a different website.
+require_once 'xlsx_excel.php';
+$firstrow=0; 
+if ( $xlsx = xlsx_excel::parse($_FILES["fileToUpload"]["tmp_name"]) ) {
+	foreach( $xlsx->rows() as $r ) {
+		if ($firstrow==0){
+// Set the column titles of your output csv file here. If there are no column titles, set "$firstrow=1" in line 7.
+			$csv[]=array("Timestamp","Action","Source","Base","DerivType","DerivDetails","Volume","Price","Counter","Fee","FeeCcy","Comment");
+			$firstrow=1;
+		}
+		else {
+// Edit the column data from the input file as needed to make your output.
+// For reference, "$r[0]" corresponds to column A on the first sheet of the xlsx file. "$r[1]" is column B and so on.
+			$csv[]=array($r[0],$r[2],"Binance",substr($r[1],0,3),"future",$r[8],$r[4],$r[3],substr($r[1],3),$r[6],$r[7],"Futures " . $r[1]);
+		}
+	}
+// Optionally change the output filename. Current format is "output-2020-09-15.csv"
+	$filename = "output-" . date('Y-m-d') . ".csv";
+	$out = fopen('php://memory', 'w');
+        	foreach( $csv as $row ) {
+			fputcsv($out, $row);
+		}
+	fseek($out, 0);
+	header('Content-Type: text/csv');
+	header('Content-Disposition: attachment; filename="' . $filename . '";');
+	fpassthru($out);
+	fclose($out);
+}
+else {
+//  Show a simple upload form if no valid xlsx sheet has been uploaded.
+?>
+<!DOCTYPE html>
+<html>
+<html lang="en">
+<title>XLSX Excel to CSV</title>
+<meta charset="UTF-8">
+<body>
+<form action="" method="post" enctype="multipart/form-data">
+  Cargue un archivo xlsx compatible:
+  <input type="file" name="fileToUpload" id="fileToUpload">
+  <input type="submit" value="Convertir Excel XLSX" name="submit">
+</form>
+</body>
+</html>
+<?php
+}
+?>
---
+++ index.php
@@ -0,0 +1,58 @@
+<?php
+?>
+<!doctype html>
+<html>		
+<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=9">
+		<title>XLSX Excel to CSV</title>
+		<meta name="description" content="Convertidor de XLSX a CSV " >
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+		<link rel="stylesheet" type="text/css" href="./css/style.css">
+
+		<!--[if lt IE 9]>
+			<script src="./js/html5shim.js"></script>
+		<![endif]-->
+	</head>
+	<body>
+		<div id="wrapper">
+			<header>
+				<div id="header-content" class="clearfix">
+					<h1><a href="#"><strong>Convertidor de XLSX a CSV</a></strong></h1>
+				</div>
+			</header>
+
+			<div id="content">
+				<p class="intro"><strong>Use esta herramienta para convertir cualquier archivo .XLSX compatible a .CSV, Puede ser útil para exportar tablas de Excel.</strong></p>
+				<ul class="indications clearfix">
+					<li><p> Elija el archivo .XLSX que desea convertir y luego presione el botón Convertir Excel XLSX</p></li>
+					<li><p> El Convertidor trabaja muy rápido y la mayoría de las veces las tablas son precisas.</p></li>
+                                        <p class="intro"><strong>Importar datos CSV en hojas de cálculo de Google</strong></p>
+					<li><p> Crear una nueva hoja de cálculo en Google Drive.</p></li>
+					<li><p> Selecciona Archivo e Importar en el menú principal.</p></li>
+					<li><p> Selecciona la pestaña Subir y el archivo CSV que exportaste desde la colección de artículos.</p></li>
+					<li><p> Cuando el archivo se haya subido, se abrirá la ventana Importar archivo. Asegúrate de desmarcar la opción «Convertir texto en números, fechas y fórmulas».</p></li>
+					<li><p> Haz clic en Importar datos para completar la importación. </p></li>
+					<li><p> <b><strong>Nota</strong></b>: Ejemplo</p></li>
+					<img src="ejemplo.png" alt="Edite sus preferencias de impuestos y modificadores desde el csv de importación de artículos">
+				</ul>
+				<p class="notice">*Aviso: El archivo se descargará automáticamente después de presionar el botón </p>
+
+
+
+					<ul class="dropdown-menu" role="menu"><li><a href="Xlsx_a_Csv_view.php?t=1655976576"><img src="../resources/table_icons/1_Excel-icon.png" height="32"> EMPIEZA A CONVERTIR</a></li></ul>
+
+
+
+				<br/>
+			</div>
+			<div id="push"></div>
+		</div>
+		<footer>
+			<div id="footer-content">
+				<a href="../index.php" target="_blank">&copy; Export XLSX</a>
+			</div>
+		</footer>
+	</body>
+</html>
